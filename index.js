29f00b545bd59119e48e4a363ff7f4ff1fa4b57c
Greeny7 <greeny7greeny@gmail.com>
2018-10-03 23:29:13 +0300

    refactoring

--- index.js
+++ index.js
@@ -3,40 +3,33 @@ const helpText = `
 --help    — печатает этот текст;
 --version — печатает версию приложения;
 `;
-
 const versionText = '0.01';
-
-const createErrorText = (command) => {return `
-Неизвестная команда ${command}.
-Чтобы прочитать правила использования приложения, наберите "--help"
-`};
-
 const defaultText = `
 Привет пользователь!
 Эта программа будет запускать сервер «Кексобукинг».
 Автор: Геннадий Белогорцев.
 `;
+const createErrorText = (command) => `
+Неизвестная команда ${command}.
+Чтобы прочитать правила использования приложения, наберите "--help"
+`;
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log(defaultText);
-  process.exit(0);
+const command = process.argv.slice(2)[0];
+switch (command) {
+  case '--version':
+    console.log(versionText);
+    process.exit(0);
+    break;
+  case '--help':
+    console.log(helpText);
+    process.exit(0);
+    break;
+  case undefined:
+    console.log(defaultText);
+    process.exit(0);
+    break;
+  default:
+    console.error(createErrorText(command));
+    process.exit(1);
+    break;
 }
-
-process.argv.slice(2).forEach(val => {
-  switch (val) {
-    case '--version':
-      console.log(versionText);
-      process.exit(0);
-      break;
-    case '--help':
-      console.log(helpText);
-      process.exit(0);
-      break;
-    default:
-      console.error(createErrorText(val));
-      process.exit(1);
-      break;
-  }
-});
