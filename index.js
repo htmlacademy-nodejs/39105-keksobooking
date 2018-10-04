cd6d0febc012df5c17dd40d0ffdad5c5dce6a381
Greeny7 <greeny7greeny@gmail.com>
2018-10-04 16:22:33 +0300

    move texts and command keys to constants

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const helpText = `
+const HELP_TEXT = `
   Доступные команды:
 --help    — печатает этот текст;
 --version — печатает версию приложения;
 `;
-const versionText = '0.01';
-const defaultText = `
+
+const VERSION = '0.01';
+
+const DEFAULT_TEXT = `
 Привет пользователь!
 Эта программа будет запускать сервер «Кексобукинг».
 Автор: Геннадий Белогорцев.
 `;
+
 const createErrorText = (command) => `
 Неизвестная команда ${command}.
 Чтобы прочитать правила использования приложения, наберите "--help"
 `;
 
+const COMMANDS = Object.freeze({
+  VERSION: '--version',
+  HELP: '--help'
+});
+
 const command = process.argv.slice(2)[0];
 switch (command) {
-  case '--version':
-    console.log(versionText);
-    process.exit(0);
+  case COMMANDS.VERSION:
+    console.log(VERSION);
     break;
-  case '--help':
-    console.log(helpText);
-    process.exit(0);
+  case COMMANDS.HELP:
+    console.log(HELP_TEXT);
     break;
   case undefined:
-    console.log(defaultText);
-    process.exit(0);
+    console.log(DEFAULT_TEXT);
     break;
   default:
     console.error(createErrorText(command));
